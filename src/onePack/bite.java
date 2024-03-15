055e996e91b2e10f5631ae391de25bb5966354bd
u.tseng <u.tseng@SIO-PC03.SIO.LOCAL>
2024-03-14 14:29:03 -1000

    Ceci est confidentiel V2

---
+++ src/onePack/bite.java
@@ -0,0 +1,14 @@
+package onePack;
+
+public class bite {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Version Alpha V1.0");
+		System.out.println("En te l'a pas dit, mais tu as un beau cul V1.1");
+		System.out.println("ptdr c'est la V1.2");
+
+		
+	}
+
+}
